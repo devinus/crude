c52baeb8904c16656faf4d2cf125277502cd111f
Devin Torres <devin@devintorr.es>
2012-12-04 10:01:00 -0600

    A finished Evented functionality

--- lib/cruddy.ex
+++ lib/cruddy.ex
@@ -4,29 +4,29 @@ defmodule Evented do
   end
 
   defmacro defevent(names) when is_list(names) do
-    lc n inlist names, do: __defevent__(n)
+    lc name inlist names, do: __defevent__(name)
   end
 
-  def __deftrigger__(event_name, callback_name) do
+  def __deftrigger__(event, callback) do
     quote do
-      def trigger(unquote(event_name), context) do
-        unquote(__trigger__(callback_name))
+      def trigger(unquote(event), context) do
+        unquote(__trigger__(callback))
       end
     end
   end
 
   defp __trigger__(callback) when is_atom(callback) do
-    quote(do: unquote(callback).(context))
+    quote do: unquote(callback).(context)
   end
 
   defp __trigger__(callbacks) when is_list(callbacks) do
-    List.foldl callbacks, [], fn cb, acc -> quote(do: unquote(cb).(unquote(acc))) end
+    List.foldl callbacks, [], fn cb, acc -> quote do: unquote(cb).(unquote(acc)) end
   end
 
-  defp __defevent__(event_name) do
+  defp __defevent__(event) do
     quote do
-      defmacro unquote(event_name)(callback_name) do
-        unquote(__MODULE__).__deftrigger__(unquote(event_name), callback_name)
+      defmacro unquote(event)(callback) do
+        unquote(__MODULE__).__deftrigger__(unquote(event), callback)
       end
     end
   end
@@ -35,35 +35,39 @@ end
 defmodule Cruddy do
   import Evented
 
+  defevent :on_create
+  defevent :on_read
+  defevent :on_update
+  defevent :on_delete
+
   defmacro fields(names) do
     Module.put_attribute __CALLER__.module, :fields, names
   end
 
-  defevent :on_create
-  # defevent :on_read
-  # defevent :on_update
-  # defevent :on_delete
-
   defmacro defcrudable(name, block) do
     quote do
       defmodule unquote(name) do
-        alias __MODULE__, as: Rec
-
-        def save(rec) do
-          rec.trigger(:on_create)
-        end
-
-        # def save(Rec[__state: :unsaved] = rec) do
-        #   rec.trigger(:on_create)
-        # end
-        #
-        # def save(Rec[__state: :saved] = rec) do
-        #   rec.trigger(:on_update)
-        # end
-
         import Cruddy
         unquote(block)
         Record.deffunctions @fields ++ [__state: :new], __ENV__
+
+        alias __MODULE__, as: Rec
+
+        def find(id) do
+          Rec.trigger(:on_read, id)
+        end
+
+        def save(Rec[__state: :new] = rec) do
+          rec.trigger(:on_create)
+        end
+
+        def save(Rec[__state: :dirty] = rec) do
+          rec.trigger(:on_update)
+        end
+
+        def delete(id) do
+          Rec.trigger(:on_delete, id)
+        end
       end
     end
   end
@@ -83,7 +87,7 @@ defmodule Test do
 
     some_event :foo
 
-    defp foo(rec) do
+    defp foo(_rec) do
       IO.puts "foo called"
     end
   end
@@ -92,10 +96,14 @@ defmodule Test do
     fields [:id, :title, :body]
 
     on_create [:do_create, :tweet_about_it, :now_do_something_crazy]
-    # on_read :do_read
+    on_read :do_read
     # on_update :do_update
     # on_delete :do_delete
-    #
+
+    defp do_read(id) do
+      __MODULE__.new id: id
+    end
+
     defp do_create(rec) do
       IO.puts "Created! Got: #{inspect rec}"
       :created
@@ -121,7 +129,7 @@ defmodule Test do
     # end
   end
 
-  post = Post.new
+  post = Post.find(42)
   post = post.title "Kurt stinks"
   post.save
   IO.inspect post.to_keywords
