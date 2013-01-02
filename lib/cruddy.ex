73fe415016f0b6855982868a9677dbb243df9066
Devin Torres <devin@devintorr.es>
2013-01-02 16:30:09 -0600

    Better tests

--- lib/cruddy.ex
+++ lib/cruddy.ex
@@ -20,7 +20,9 @@ defmodule Evented do
   end
 
   defp __trigger__(callbacks) when is_list(callbacks) do
-    List.foldl callbacks, [], fn cb, acc -> quote do: unquote(cb).(unquote(acc)) end
+    List.foldl callbacks, quote(do: context), fn cb, acc ->
+      quote do: unquote(cb).(unquote(acc))
+    end
   end
 
   defp __defevent__(event) do
@@ -39,17 +41,26 @@ defmodule Cruddy do
   defevent :on_read
   defevent :on_update
   defevent :on_delete
+  defevent :on_exists?
 
   defmacro fields(names) do
     Module.put_attribute __CALLER__.module, :fields, names
+  end
+
+  defmacro primary_key(name) do
+    Module.put_attribute __CALLER__.module, :primary_key, name
   end
 
   defmacro defcrudable(name, block) do
     quote do
       defmodule unquote(name) do
         import Cruddy
+
+        @primary_key :id
+        @fields [:id]
+
         unquote(block)
-        Record.deffunctions @fields ++ [__state: :new], __ENV__
+        Record.deffunctions @fields, __ENV__
 
         alias __MODULE__, as: Rec
 
@@ -57,80 +68,21 @@ defmodule Cruddy do
           Rec.trigger(:on_read, id)
         end
 
-        def save(Rec[__state: :new] = rec) do
-          rec.trigger(:on_create)
+        def exists?(rec) do
+          Rec.trigger(:on_exists?, rec[@primary_key])
         end
 
-        def save(Rec[__state: :dirty] = rec) do
-          rec.trigger(:on_update)
+        def save(rec) do
+          case Rec.trigger(:on_exists?, rec[@primary_key]) do
+            true -> rec.trigger(:on_update)
+            false -> rec.trigger(:on_create)
+          end
         end
 
-        def delete(id) do
-          Rec.trigger(:on_delete, id)
+        def delete(rec) do
+          Rec.trigger(:on_delete, rec[@primary_key])
         end
       end
     end
   end
-end
-
-defmodule Test do
-  import Cruddy
-
-  defmodule MyEvents do
-    import Evented
-
-    defevent :some_event
-  end
-
-  defmodule Thing do
-    import MyEvents
-
-    some_event :foo
-
-    defp foo(_rec) do
-      IO.puts "foo called"
-    end
-  end
-
-  defcrudable Post do
-    fields [:id, :title, :body]
-
-    on_create [:do_create, :tweet_about_it, :now_do_something_crazy]
-    on_read :do_read
-    # on_update :do_update
-    # on_delete :do_delete
-
-    defp do_read(id) do
-      __MODULE__.new id: id
-    end
-
-    defp do_create(rec) do
-      IO.puts "Created! Got: #{inspect rec}"
-      :created
-    end
-
-    defp tweet_about_it(rec) do
-      IO.puts "Tweeted! Got: #{inspect rec}"
-      :tweeted
-    end
-
-    defp now_do_something_crazy(rec) do
-      IO.puts "Something crazy! Got: #{inspect rec}"
-      :something_crazy
-    end
-
-    # defp do_read(rec) do
-    # end
-    #
-    # defp do_update(rec) do
-    # end
-    #
-    # defp do_delete(rec) do
-    # end
-  end
-
-  post = Post.find(42)
-  post = post.title "Kurt stinks"
-  post.save
-  IO.inspect post.to_keywords
 end
--- test/cruddy_test.exs
+++ test/cruddy_test.exs
@@ -3,7 +3,82 @@ Code.require_file "../test_helper.exs", __FILE__
 defmodule CruddyTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert true
+  import Cruddy
+
+  defmodule Events do
+    import Evented
+
+    defevent :incr
+    defevent :decr
+    defevent :sq
+    defevent :algo
+  end
+
+  defmodule Calculator do
+    import Events
+
+    incr :add_1
+    decr :sub_1
+    sq :exp_2
+    algo [:add_1, :exp_2, :sub_1]
+
+    defp add_1(n) do
+      IO.puts "adding #{inspect n}"
+      n + 1
+    end
+
+    defp sub_1(n) do
+      IO.puts "subtracting #{inspect n}"
+      n - 1
+    end
+
+    defp exp_2(n) do        
+      IO.puts "exponentiating #{inspect n}"
+      n * n
+    end
+  end
+
+  defcrudable Post do
+    fields [:id, :title, :body, :state]
+
+    on_create :do_create
+    on_read :do_read
+    on_update :do_update
+    on_delete :do_delete
+    on_exists? :do_exists
+
+    alias __MODULE__, as: Rec
+
+    defp do_exists(Rec[state: :new] = rec), do: false
+    defp do_exists(Rec[state: :created] = rec), do: true
+
+    defp do_create(rec) do
+      rec.state :created
+    end
+
+    defp do_read(id) do
+      __MODULE__.new id: id, state: :new
+    end
+    
+    defp do_update(rec), do: rec
+    
+    defp do_delete(rec), do: true
+  end
+
+  test :evented do
+    assert Calculator.trigger(:incr, 1) == 2
+    assert Calculator.trigger(:decr, 4) == 3
+    assert Calculator.trigger(:sq,   2) == 4
+    assert Calculator.trigger(:algo, 3) == 15
+  end
+
+  test :read do
+    post = Post.find(123)
+    assert post.id == 123
+  end
+
+  test :exists? do
+    post = Post.find(123)
+    assert not post.exists?
   end
 end
